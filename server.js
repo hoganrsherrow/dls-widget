8677bfeef38ed4685b86721a9133997401d348db
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-10-25 15:16:46 -0500

    added middleware, routed to controllers, setup sequelize configuration

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 3001;
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+const PORT = process.env.PORT || 3001;
 
 
 // Express Middleware()
@@ -13,7 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// sends to controllers for routing
+app.use(require('./controllers/'));
+
 // Start up the server
 app.listen(PORT, () => {
     console.log(`DLS-WIDGET now listening on port ${PORT}.`);
-})
+});
